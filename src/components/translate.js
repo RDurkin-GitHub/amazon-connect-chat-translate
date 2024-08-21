9971bb7ce55a9fa31bea87fdbd6fa3579383adf4
RDurkin-GitHub <102746837+RDurkin-GitHub@users.noreply.github.com>
2024-08-21 13:58:17 +0100

    Update translate.js

--- src/components/translate.js
+++ src/components/translate.js
@@ -12,9 +12,6 @@ async function ProcessChatText(content, sourceLang, tagretLang) {
                 // supported languages https://docs.aws.amazon.com/translate/latest/dg/how-it-works.html#how-it-works-language-codes
             },
             targetLanguage: tagretLang,
-            Settings: {
-                Formality: 'FORMAL'
-        }
     });
     return transcriptMessage.text
 }
